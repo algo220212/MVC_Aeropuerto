cf0752711f163a63df5a9eab6b0031c38ea2b984
OCA-UIE <OCA-UIE@DESKTOP-AHIE2HK>
2021-12-01 23:02:03 -0500

    Proyecto MVC

---
+++ src/main/java/algoritmica/com/mavenproject1/App.java
@@ -0,0 +1,28 @@
+
+package algoritmica.com.mavenproject1;
+
+import java.util.Date;
+import modelo.Aeropuerto;
+import modelo.Ciudad;
+
+public class App {
+    public static void main(String[] args) {
+       
+        
+        Sistema.paises.agegarPais("PE", "PERÚ");
+        Sistema.paises.agegarPais("AR", "ARGENTINA");
+        Sistema.paises.buscarPais("PE").agregarCiudad("L001","Lima");
+        Sistema.paises.buscarPais("AR").agregarCiudad("A001","BUENOS AIRES");
+        Ciudad ciudad =  Sistema.paises.buscarPais("PE").buscarCiudad("L001");
+        Sistema.aeropuertos.agrgarAeropuerto("JORGE CHAVEZ", "Av. Elmer Foucett S/N", ciudad);
+
+        ciudad =  Sistema.paises.buscarPais("AR").buscarCiudad("A001");
+        Sistema.aeropuertos.agrgarAeropuerto("RIO DE LA PLATA", "Av. Buenos Aires S/N", ciudad);
+        
+        Aeropuerto salida = Sistema.aeropuertos.buscarAeropuerto("JORGE CHAVEZ");
+        Aeropuerto llegada = Sistema.aeropuertos.buscarAeropuerto("RIO DE LA PLATA");
+        Sistema.vuelos.agregarVuelo("V0001", new Date(), salida, llegada, 200 );
+        
+    }
+    
+}
